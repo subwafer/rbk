f01b5c1e2b7559e9991bc855f7353dc76ddda3e0
subwafer <subwafer@proton.me>
2023-02-04 22:01:12 -0500

    implemented a create_readme_file function

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void create_readme_file(void) {
+    FILE *file;
+
+    file = fopen("test_README.md", "w");
+    if (file == NULL) {
+        printf("ERROR: Unable to create file\n");
+        exit(1);
+    }
+
+    fputs("# Project Name\n\n## Abstract\n\n## Author(s)\n", file);
+
+}
 
 int main(void) {
-    char hello[] = "Hello, World!";
-
-    printf("%s\n", hello);
+    create_readme_file();
 
     return 0;
 }
