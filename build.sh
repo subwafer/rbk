67b6872145b8c07f61b9512df34ff5a82599382d
subwafer <subwafer@proton.me>
2023-02-19 11:57:16 -0500

    updated build.sh to handle flags, etc.

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
-gcc -o main main.c
+#!/bin/sh
+
+set -xe
+
+# CC=clang
+# CFLAGS="-Wall -Wextra -std=c11 -pedantic -ggdb -fsanitize=memory"
+
+CC=gcc
+CFLAGS="-Wall -Wextra -std=c11 -pedantic -ggdb"
+
+$CC $CFLAGS -o main main.c
 
 ./main --debug
